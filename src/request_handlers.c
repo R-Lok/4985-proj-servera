e1724bdf03bffd863d53164987ce4eb9d7ba6dd7
Ryan Lok <rlok.pc@gmail.com>
2025-02-08 00:44:46 -0800

    Fix logical error

--- src/request_handlers.c
+++ src/request_handlers.c
@@ -11,7 +11,9 @@ int handle_login(HandlerArgs *args, int fd);
 int handle_logout(HandlerArgs *args, int fd);
 int handle_acc_create(HandlerArgs *args, int fd);
 
-int extract_login_fields(uint16_t reported_payload_length, char *p_buffer, char *name, char *password);
+// int extract_login_fields(uint16_t reported_payload_length, char *p_buffer, char *name, char *password);
+
+int extract_field(char *payload_ptr, void *buffer, uint16_t *byte_threshold, uint8_t ber_tag);
 
 RequestHandler get_handler_function(uint8_t packet_type)
 {
@@ -35,21 +37,28 @@ RequestHandler get_handler_function(uint8_t packet_type)
 
 int handle_login(HandlerArgs *args, int fd)
 {
-    char username[NAME_BUFFER_SIZE];
-    char password[PASSWORD_BUFFER_SIZE];
-    int  extract_res;
-    int  ret;
+    char     username[NAME_BUFFER_SIZE];
+    char     password[PASSWORD_BUFFER_SIZE];
+    int      ret;
+    uint16_t remaining_bytes;
+
+    remaining_bytes = args->hd->payload_len;
 
     ret = 0;
 
-    extract_res = extract_login_fields(args->hd->payload_len, args->payload_buffer, username, password);
-    if(extract_res == 1 || extract_res == 2)
+    if(extract_field(args->payload_buffer, username, &remaining_bytes, P_UTF8STRING))
     {
-        send_sys_error(fd, P_BAD_REQUEST, P_BAD_REQUEST_MSG);
-        return 0;    // ok error, client had bad request
+        send_sys_error(fd, P_BAD_REQUEST, P_BAD_REQUEST_MSG);    // need error handling
+        return 0;
     }
 
-    printf("%s:%s\n", username, password);    // remove this later;
+    if(extract_field(args->payload_buffer, password, &remaining_bytes, P_UTF8STRING))
+    {
+        send_sys_error(fd, P_BAD_REQUEST, P_BAD_REQUEST_MSG);    // need error handling
+        return 0;
+    }
+
+    printf("%s:%s | remaining bytes: %u\n", username, password, remaining_bytes);    // remove this later;
 
     // call try_login() - this function would do DB calls for the username (key), if nothing returned, or value (password) does not match, error
     // try_login would also be responsible for updating sd->fd_map
@@ -58,49 +67,81 @@ int handle_login(HandlerArgs *args, int fd)
     return ret;
 }
 
-int extract_login_fields(uint16_t reported_payload_length, char *p_buffer, char *name, char *password)
+int extract_field(char *payload_ptr, void *buffer, uint16_t *byte_threshold, uint8_t ber_tag)
 {
     const int SUCCESS                 = 0;
     const int INCORRECT_FIELD_TYPE    = 1;
     const int PAYLOAD_LENGTH_MISMATCH = 2;
-    int       payload_length_sum;
-    uint8_t   name_len;
-    uint8_t   password_len;
+    char     *buffer_ptr;
+    uint8_t   data_len;
 
-    payload_length_sum = 0;
-    // Check BER to be UTF8STRING
-    if(*p_buffer++ != P_UTF8STRING)
-    {    // shift to length after check
-        return INCORRECT_FIELD_TYPE;
-    }
+    buffer_ptr = (char *)buffer;
 
-    name_len = *((uint8_t *)(p_buffer));                 // get name_length
-    payload_length_sum += *((uint8_t *)(p_buffer++));    // increment sum and move ptr to first char of name
-    if(payload_length_sum > reported_payload_length)
+    if(*payload_ptr++ != (char)ber_tag)
     {
-        return PAYLOAD_LENGTH_MISMATCH;
-    }
-    memcpy(name, p_buffer, name_len);
-    name[name_len] = '\0';
-
-    p_buffer += name_len;    // increment ptr to next BER tag
-    if(*p_buffer++ != P_UTF8STRING)
-    {    // check and move pointer
         return INCORRECT_FIELD_TYPE;
     }
 
-    password_len = *((uint8_t *)(p_buffer));
-    payload_length_sum += *((uint8_t *)(p_buffer++));    // increment to first char of password;
-    if(payload_length_sum > reported_payload_length)
+    data_len = *(uint8_t *)(payload_ptr++);
+
+    if(data_len > *byte_threshold - 2)
     {
         return PAYLOAD_LENGTH_MISMATCH;
     }
 
-    memcpy(password, p_buffer, password_len);
-    password[password_len] = '\0';
+    memcpy(buffer_ptr, payload_ptr, data_len);
+    if(ber_tag == P_UTF8STRING)
+    {
+        buffer_ptr[data_len] = '\0';
+    }
+    *byte_threshold = (uint16_t)(*byte_threshold - data_len - 2);
 
     return SUCCESS;
 }
+
+// int extract_login_fields(uint16_t reported_payload_length, char *p_buffer, char *name, char *password)
+// {
+//     const int SUCCESS                 = 0;
+//     const int INCORRECT_FIELD_TYPE    = 1;
+//     const int PAYLOAD_LENGTH_MISMATCH = 2;
+//     int       payload_length_sum;
+//     uint8_t   name_len;
+//     uint8_t   password_len;
+
+//     payload_length_sum = 0;
+//     // Check BER to be UTF8STRING
+//     if(*p_buffer++ != P_UTF8STRING)
+//     {    // shift to length after check
+//         return INCORRECT_FIELD_TYPE;
+//     }
+
+//     name_len = *((uint8_t *)(p_buffer));                 // get name_length
+//     payload_length_sum += *((uint8_t *)(p_buffer++));    // increment sum and move ptr to first char of name
+//     if(payload_length_sum > reported_payload_length)
+//     {
+//         return PAYLOAD_LENGTH_MISMATCH;
+//     }
+//     memcpy(name, p_buffer, name_len);
+//     name[name_len] = '\0';
+
+//     p_buffer += name_len;    // increment ptr to next BER tag
+//     if(*p_buffer++ != P_UTF8STRING)
+//     {    // check and move pointer
+//         return INCORRECT_FIELD_TYPE;
+//     }
+
+//     password_len = *((uint8_t *)(p_buffer));
+//     payload_length_sum += *((uint8_t *)(p_buffer++));    // increment to first char of password;
+//     if(payload_length_sum > reported_payload_length)
+//     {
+//         return PAYLOAD_LENGTH_MISMATCH;
+//     }
+
+//     memcpy(password, p_buffer, password_len);
+//     password[password_len] = '\0';
+
+//     return SUCCESS;
+// }
 
 // This will get rewritten - was only written so we could test with a client group - it successfully printed out the username and password.
 // int handle_login(HandlerArgs *args, int fd)
